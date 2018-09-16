01b2adf9b02013b9bebfb34312271f426d9fec62
Veronica Bustabad <vbustabad@gmail.com>
2018-09-16 20:44:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-function preventRefreshOnSubmit(){
+function preventRefreshOnSubmit() {
     document.querySelector('form').addEventListener('submit', function(event){
       event.preventDefault()
     })
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  const input = document.querySelector('input').value;
+
+  return input;
+}
+
+function addNewElementAsLi() {
+  const input = document.querySelector('input').value;
+}
+
+function addNewLiOnClick() {
+}
+
+function clearEmployeeListOnLinkClick() {
+}
