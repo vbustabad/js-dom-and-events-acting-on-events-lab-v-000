1d95fc6dc8f9e0b918aa1ec4ce7589534702e4cf
Veronica Bustabad <vbustabad@gmail.com>
2018-09-17 00:59:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-function preventRefreshOnSubmit(){
+function preventRefreshOnSubmit() {
     document.querySelector('form').addEventListener('submit', function(event){
       event.preventDefault()
     })
 }
 
 preventRefreshOnSubmit()
+
+function retrieveEmployeeInformation() {
+  const input = document.querySelector("input").value;
+  return input;
+}
+
+function addNewElementAsLi() {
+  const input = retrieveEmployeeInformation();
+  document.querySelector(".employee-list").innerHTML = input;
+}
+
+function addNewLiOnClick() {
+  document.querySelector("form").addEventListener('click', addNewElementAsLi());
+  document.querySelector("input").value = '';
+}
+
+function clearEmployeeListOnLinkClick() {
+  let ul = document.querySelector("ul");
+  let button = document.querySelector("a");
+
+  button.addEventListener('click', function(event) {
+    ul.innerHTML = '';
+  });
+}
